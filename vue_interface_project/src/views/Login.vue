decf3621f9709b1a6530b3a3366531e7e9a6248d
Wenchen115 <1243103813@qq.com>
2019-09-01 22:35:54 +0800

    登陆前端

--- vue_interface_project/src/views/Login.vue
+++ vue_interface_project/src/views/Login.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="login">
-    <div class="main-head">
-      <div class="head-img">
-        <img :src ='vueImg' height="50px" width="50px"/>
-        <span>接口测试系统</span>
-      </div>
-    </div>
+    <div class="login">
+        <header-component></header-component>
 
     <div class="login-context">
       <h2>账号登陆</h2>
-      <div>
-        <el-input v-model="loginForm.name" placeholder="请输入用户名"></el-input>
-        <el-input placeholder="请输入密码" v-model="loginForm.passWord" show-password></el-input>
-      </div>
+        <el-form label-position="left" :model="loginForm" :rules="rules" ref="loginFormRef" label-width="70px" class="demo-ruleForm">
+            <el-form-item label="用户名" prop="name">
+                <el-input v-model="loginForm.name" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="passWord">
+                <el-input v-model="loginForm.passWord" placeholder="请输入密码" show-password></el-input>
+            </el-form-item>
+        </el-form>
 
       <div class="login-button-class">
         <el-button type="success" @click="login">登陆</el-button>
@@ -25,10 +24,11 @@
 </template>
 
 <script>
-  import img from "../assets/logo.png"
+    import header from "../components/header"
   export default {
     name:'login',
     components:{
+        "header-component":header
 
     },
     props:{
@@ -36,16 +36,52 @@
     },
     data(){
       return{
-        vueImg:img,
 
         loginForm:{
           name:"",
           passWord:"",
+        },
+        rules: {
+            name: [
+                {required: true, message: '请输入用户名称', trigger: 'blur'},
+                {min: 1, max: 30, message: '长度在 3 到 5 个字符', trigger: 'blur'}
+            ],
+            passWord: [
+                {required: true, message: '请输入密码', trigger: 'blur'},
+                {min: 1, max: 30, message: '长度在 1 到 30 个字符', trigger: 'blur'}
+            ],
         }
 
       }
     },
     methods:{
+        // validForm(){
+        //     if(""==this.loginForm.name){
+        //         alert("用户名不能为空")
+        //         return
+        //     }
+        //     if(""==this.loginForm.passWord){
+        //         alert("密码不能为空")
+        //         return
+        //     }
+
+        // },
+      login: function () {
+          this.$refs.loginFormRef.validate((valid) => {
+                  if (valid) {
+                      // alert('校验通过');
+                  } else {
+                      // alert('校验失败!');
+                      return false;
+                  }
+              });
+              this.validForm();
+
+      },
+        register(){
+            this.validForm();
+
+        },
 
     },
     created() {
@@ -55,19 +91,6 @@
 </script>
 
 <style scoped>
-。main-head{
-  background: #eff4fa;
-  height: 60px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-}
-
-.head-img{
-  display: flex;
-  align-items: center;
-}
 
 .login-context{
   width: 400px;
@@ -75,9 +98,7 @@
   margin-right: auto;
   border-radius: 2px;
   border: 1px solid #a0b1c4;
-
   margin-top: 50px;
-
   padding: 10px 30px;
 
 }
@@ -85,6 +106,5 @@
 .login-button-class{
   display: flex;
   justify-content: space-between;
-  padding-left: 25px;
 }
 </style>
